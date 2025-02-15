a9d06f5b9859a6f29a19f6cdf2f82ea52c8381b6
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-15 13:34:09 -0300

    useRef para controlar primer renderizado de la página

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
@@ -6,8 +6,14 @@ import { useMovies } from "./hooks/useMovies";
 function useSearch() {
   const [search, updateSearch] = useState("");
   const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
 
   useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+
     if (search == "") {
       setError("No se puede buscar una película vacía.");
       return;
