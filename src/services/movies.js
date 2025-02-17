24c06a5f9c0f9c31068ffc6c8abde97572302f3c
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-17 08:56:41 -0300

    services reemplazando logica en useMovies

--- src/services/movies.js
+++ src/services/movies.js
@@ -10,12 +10,14 @@ export const searchMovies = async ({ search }) => {
     const json = await response.json();
     const movies = json.Search;
 
-    const mappedMovies = movies?.map((movie) => ({
+    return movies?.map((movie) => ({
       id: movie.imdbID,
       title: movie.Title,
       year: movie.Year,
       type: movie.Type,
       poster: movie.Poster,
     }));
-  } catch (error) {}
+  } catch (error) {
+    throw new Error("Error searching movies", error);
+  }
 };
