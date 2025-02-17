e853845d85d257a39d96a9e77ffdf39f86576f01
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-17 08:47:00 -0300

    agregando services

---
+++ src/services/movies.js
@@ -0,0 +1,21 @@
+const apiKey = import.meta.env.VITE_API_KEY;
+
+export const searchMovies = async ({ search }) => {
+  if (search == "") return null;
+
+  try {
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${apiKey}&s=${search}`
+    );
+    const json = await response.json();
+    const movies = json.Search;
+
+    const mappedMovies = movies?.map((movie) => ({
+      id: movie.imdbID,
+      title: movie.Title,
+      year: movie.Year,
+      type: movie.Type,
+      poster: movie.Poster,
+    }));
+  } catch (error) {}
+};
